430a06615cec11f19f3d4d6cfdc310568c473d5c
pratikgon96 <85388957+pratikgon96@users.noreply.github.com>
2021-09-03 16:16:51 +0530

    Add files via upload

---
+++ Assignment/Models/ProjectEmployee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assignment.Models
+{
+    public class ProjectEmployee
+    {
+        [Required]
+        public int ProjectId { get; set; }
+        [Required]
+        public int EmployeeId { get; set; }
+        public Project project { get; set; }
+        public Employee employee { get; set; }
+    }
+}
